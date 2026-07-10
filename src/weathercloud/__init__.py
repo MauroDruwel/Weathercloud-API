93deecacc14eb2f3d12193c14b249df7835a56ce
Mauro Druwel <codermauro@users.noreply.github.com>
2026-07-10 22:00:25 +0200

    chore: bump version to 0.1.3

--- src/weathercloud/__init__.py
+++ src/weathercloud/__init__.py
@@ -16,7 +16,7 @@ from .client import WeathercloudClient
 from .exceptions import WeathercloudError
 from .models import CurrentConditions, StationInfo, VariableCode
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = [
     "WeathercloudClient",
     "WeathercloudError",
